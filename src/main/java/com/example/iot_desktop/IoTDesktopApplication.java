898522c7cbcbb1f424424feaaf120f53451e9875
Kamil Rataj <kamilr616@gmail.com>
2023-03-30 09:52:35 +0200

    Initial commit

---
+++ src/main/java/com/example/iot_desktop/IoTDesktopApplication.java
@@ -0,0 +1,13 @@
+package com.example.iot_desktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTDesktopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTDesktopApplication.class, args);
+	}
+
+}
