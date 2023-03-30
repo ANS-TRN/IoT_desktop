94049e26e156df702cb86e43b2484bf5d35e4cc5
Kamil Rataj <kamilr616@gmail.com>
2023-03-30 09:54:36 +0200

    1

--- src/main/java/com/example/iot_desktop/IoTDesktopApplication.java
+++ src/main/java/com/example/iot_desktop/IoTDesktopApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IoTDesktopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(IoTDesktopApplication.class, args);
+
 	}
 
 }
